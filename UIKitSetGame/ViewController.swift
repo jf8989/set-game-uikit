514b87fedcc1ca9dea0f2cb1017b339ef1992fb2
Juan Francisco Marcenaro Arellano <128626952+jf8989@users.noreply.github.com>
2025-09-18 09:07:11 -0500

    Initialize UIKitSetGame project structure with essential files and configurations

---
+++ UIKitSetGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitSetGame
+//
+//  Created by Juan Francisco Marcenaro Arellano on 18/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
